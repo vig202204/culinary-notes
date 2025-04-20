88722dc49db275dd30105f714e20d978b77b8358
ihorvoloshyn <vig092007@gmail.com>
2025-04-20 18:20:16 +0300

    initial commit

---
+++ src/main/java/edada/com/ua/culinarynotes/CulinaryNotesApplication.java
@@ -0,0 +1,13 @@
+package edada.com.ua.culinarynotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulinaryNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CulinaryNotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edada/com/ua/culinarynotes/CulinaryNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package edada.com.ua.culinarynotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulinaryNotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
